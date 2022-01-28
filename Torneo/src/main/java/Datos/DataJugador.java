2d63c4139b1bc7f9cde92e9f8c837d0bd019c633
SSavrinkha <santallasabrina@gmail.com>
2022-01-28 03:38:02 -0300

    Agrego listado jugador por juego y errores fix

--- Torneo/src/main/java/Datos/DataJugador.java
+++ Torneo/src/main/java/Datos/DataJugador.java
@@ -12,15 +12,15 @@ import Entidades.Jugador;
 import Entidades.Pais;
 
 public class DataJugador {
-	
-public void nuevo(Jugador nuevoJugador) {
-		
+
+	public void nuevo(Jugador nuevoJugador) {
+
 		Connection conn = null;
-		
+
 		ResultSet keyrs = null;
-		
+
 		PreparedStatement stmt = null;
-		
+
 		try {
 			conn = DbConnector.getInstancia().getConn();
 			stmt = conn.prepareStatement("INSERT INTO jugadores (usuario, nombre, apellido, contraseña, id_pais) VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
@@ -31,12 +31,12 @@ public void nuevo(Jugador nuevoJugador) {
 			stmt.setInt(5, nuevoJugador.getPais().getId());
 			stmt.executeUpdate();
 			keyrs = stmt.getGeneratedKeys();
-			
+
 			if (keyrs != null && keyrs.next()) {
-				
+
 				nuevoJugador.setId(keyrs.getInt(1));
 			}	
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -44,60 +44,60 @@ public void nuevo(Jugador nuevoJugador) {
 				if (conn != null) conn.close();
 				if (stmt != null) stmt.close();
 				if (keyrs != null) keyrs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
-//public void create(int id, String usuario, String contraseña, String nombre, String apellido) {
-//		
-//		Connection conn = null;
-//		PreparedStatement pstmt = null;
-//		Jugador jNuevo = new Jugador();
-//		
-//		try {
-//			// crear conexion
-//			conn = DbConnector.getInstancia().getConn();
-//			MessageDigest md = MessageDigest.getInstance("SHA-256");
-//			jNuevo.setId(id);
-//			jNuevo.setUsuario(usuario);
-//			jNuevo.setContraseña( md.digest(contraseña.getBytes()).toString() );
-//			jNuevo.setNombre(nombre);
-//			jNuevo.setApellido(apellido);
-//			
-//			//query
-//			pstmt = conn.prepareStatement(
-//			"INSERT INTO jugadores(id, usuario, nombre, apellido, contraseña) VALUES(?,?,?,?,?)");
-//			
-//			pstmt.setInt(1, jNuevo.getId());
-//			pstmt.setString(2, jNuevo.getUsuario());
-//			pstmt.setString(3, jNuevo.getNombre());
-//			pstmt.setString(4, jNuevo.getApellido());
-//			pstmt.setString(5, jNuevo.getContraseña());
-//			pstmt.executeUpdate();
-//			
-//			if(pstmt!=null) {pstmt.close();}
-//			conn.close();
-//			
-//		}catch(SQLException | NoSuchAlgorithmException ex) {
-//			System.out.println("SQLException: " + ex.getMessage());
-//		}finally {
-//			try {
-//			if(pstmt!=null) {pstmt.close();}
-//			conn.close();
-//			}catch(SQLException e){
-//				e.printStackTrace();
-//			}
-//		}
-//	}
-	
+
+	//public void create(int id, String usuario, String contraseña, String nombre, String apellido) {
+	//		
+	//		Connection conn = null;
+	//		PreparedStatement pstmt = null;
+	//		Jugador jNuevo = new Jugador();
+	//		
+	//		try {
+	//			// crear conexion
+	//			conn = DbConnector.getInstancia().getConn();
+	//			MessageDigest md = MessageDigest.getInstance("SHA-256");
+	//			jNuevo.setId(id);
+	//			jNuevo.setUsuario(usuario);
+	//			jNuevo.setContraseña( md.digest(contraseña.getBytes()).toString() );
+	//			jNuevo.setNombre(nombre);
+	//			jNuevo.setApellido(apellido);
+	//			
+	//			//query
+	//			pstmt = conn.prepareStatement(
+	//			"INSERT INTO jugadores(id, usuario, nombre, apellido, contraseña) VALUES(?,?,?,?,?)");
+	//			
+	//			pstmt.setInt(1, jNuevo.getId());
+	//			pstmt.setString(2, jNuevo.getUsuario());
+	//			pstmt.setString(3, jNuevo.getNombre());
+	//			pstmt.setString(4, jNuevo.getApellido());
+	//			pstmt.setString(5, jNuevo.getContraseña());
+	//			pstmt.executeUpdate();
+	//			
+	//			if(pstmt!=null) {pstmt.close();}
+	//			conn.close();
+	//			
+	//		}catch(SQLException | NoSuchAlgorithmException ex) {
+	//			System.out.println("SQLException: " + ex.getMessage());
+	//		}finally {
+	//			try {
+	//			if(pstmt!=null) {pstmt.close();}
+	//			conn.close();
+	//			}catch(SQLException e){
+	//				e.printStackTrace();
+	//			}
+	//		}
+	//	}
+
 	//Delete otro
 	public void borrar(Jugador borrarJugador) {
-	
+
 		Connection conn = null;
 		PreparedStatement stmt = null;
-	
+
 		try {
 			conn = DbConnector.getInstancia().getConn();
 			stmt = conn.prepareStatement("delete from tipo_torneo where usuario = ?");
@@ -109,50 +109,50 @@ public void nuevo(Jugador nuevoJugador) {
 			try {
 				if(conn != null) conn.close();
 				if(stmt != null) stmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-	
+
 	}
 
 	//borrar
-//	public void delete(String usuario) {
-//		
-//		Connection conn = null;
-//		PreparedStatement pstmt = null;
-//		
-//		try {
-//			// crear conexion
-//			conn = DbConnector.getInstancia().getConn();
-//			
-//			//query
-//			pstmt = conn.prepareStatement(
-//			"DELETE FROM jugadores WHERE usuario = ?" 
-//					);
-//			
-//			pstmt.setString(1,usuario);
-//			pstmt.executeUpdate();
-//			
-//			if(pstmt!=null) {pstmt.close();}
-//			conn.close();
-//			
-//		}catch(SQLException ex) {
-//			System.out.println("SQLException: " + ex.getMessage());
-//		}finally {
-//			try {
-//			if(pstmt!=null) {pstmt.close();}
-//			conn.close();
-//			}catch(SQLException e){
-//				e.printStackTrace();
-//			}
-//		}
-//		
-//	}
-	
+	//	public void delete(String usuario) {
+	//		
+	//		Connection conn = null;
+	//		PreparedStatement pstmt = null;
+	//		
+	//		try {
+	//			// crear conexion
+	//			conn = DbConnector.getInstancia().getConn();
+	//			
+	//			//query
+	//			pstmt = conn.prepareStatement(
+	//			"DELETE FROM jugadores WHERE usuario = ?" 
+	//					);
+	//			
+	//			pstmt.setString(1,usuario);
+	//			pstmt.executeUpdate();
+	//			
+	//			if(pstmt!=null) {pstmt.close();}
+	//			conn.close();
+	//			
+	//		}catch(SQLException ex) {
+	//			System.out.println("SQLException: " + ex.getMessage());
+	//		}finally {
+	//			try {
+	//			if(pstmt!=null) {pstmt.close();}
+	//			conn.close();
+	//			}catch(SQLException e){
+	//				e.printStackTrace();
+	//			}
+	//		}
+	//		
+	//	}
+
 	//actualizar
 	public void update(int id, String usuario, String contraseña, String nombre, String apellido) {
-		
+
 		PreparedStatement pstmt = null;
 		Connection conn = null;
 		Jugador jNuevo = new Jugador();
@@ -161,29 +161,29 @@ public void nuevo(Jugador nuevoJugador) {
 		jNuevo.setContraseña(contraseña);
 		jNuevo.setNombre(nombre);
 		jNuevo.setApellido(apellido);
-		
+
 		try {
 			// crear conexion
 			conn = DbConnector.getInstancia().getConn();
-			
+
 			//query
 			pstmt = conn.prepareStatement(
-			"Update jugadores "
-			+ "SET usuario=?, contraseña=?, nombre=?, apellido=?"
-			+ "WHERE id=? " 
+					"Update jugadores "
+							+ "SET usuario=?, contraseña=?, nombre=?, apellido=?"
+							+ "WHERE id=? " 
 					);
-			
+
 			pstmt.setInt(5, jNuevo.getId());
 			pstmt.setString(1, jNuevo.getUsuario());
 			pstmt.setString(2, jNuevo.getUsuario());
 			pstmt.setString(3, jNuevo.getNombre());
 			pstmt.setString(4, jNuevo.getApellido());
 			pstmt.executeQuery();
-			
+
 			if(pstmt!=null) {pstmt.close();}
 			conn.close();
-			
-			
+
+
 		}catch(SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
 		}finally {
@@ -194,28 +194,28 @@ public void nuevo(Jugador nuevoJugador) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
 	public Jugador login(String usuario, String contraseña) {
-		
+
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		Jugador j = new Jugador();
-		
+
 		try {
 			// conexion
 			conn = DbConnector.getInstancia().getConn();
-			
+
 			stmt = conn.prepareStatement("SELECT * FROM Jugadores j WHERE j.usuario = ? AND j.contraseña = ?");
 			//setear parametros
 			stmt.setString(1, usuario);
 			stmt.setString(2, contraseña);
-			
+
 			//resultados
 			rs = stmt.executeQuery();
-			
+
 			//mapear
 			if(rs.next()) {
 				j.setId(rs.getInt("id"));
@@ -224,13 +224,13 @@ public void nuevo(Jugador nuevoJugador) {
 				j.setUsuario(rs.getString("usuario"));
 				j.setContraseña(rs.getString("contraseña"));
 			}
-			
+
 			//cerrar conexion
 			if(rs!=null) {rs.close();}
 			if(stmt!=null) {stmt.close();}
-			
+
 			conn.close();
-			
+
 		}catch(SQLException ex){
 			System.out.println("SQLException: " + ex.getMessage());
 		}finally {
@@ -244,26 +244,26 @@ public void nuevo(Jugador nuevoJugador) {
 		}
 		return j;
 	}
-	
-public LinkedList<Jugador> listaPais(String nombrePais) throws SQLException {
-		
+
+	public LinkedList<Jugador> listaPais(String nombrePais) throws SQLException {
+
 		Connection conn = null;
-		
+
 		ResultSet rs = null;
-		
+
 		PreparedStatement stmt = null;
-		
+
 		try {
-			
+
 			LinkedList<Jugador> jugadores = new LinkedList<Jugador>();
 			conn = DbConnector.getInstancia().getConn();
 			stmt = conn.prepareStatement("select * from jugadores j inner join paises p on p.id = j.id_pais where p.nombre = ?");
 			stmt.setString(1, nombrePais);
 			rs = stmt.executeQuery();
 			if (rs != null) {
-				
+
 				while (rs.next()) {
-					
+
 					Jugador jugador = new Jugador();
 					Pais pais = new Pais();
 					pais.setId(rs.getInt("id_pais"));
@@ -282,14 +282,65 @@ public LinkedList<Jugador> listaPais(String nombrePais) throws SQLException {
 			return null;
 		} finally {
 			try {
-			if (rs != null) rs.close();
-			if (stmt!=null) stmt.close();
-			if (conn!=null) conn.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
+				if (rs != null) rs.close();
+				if (stmt!=null) stmt.close();
+				if (conn!=null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public LinkedList<Jugador> listaJugadoresJuego(String nombreJuego) throws SQLException {
+
+		Connection conn = null;
+
+		ResultSet rs = null;
+
+		PreparedStatement stmt = null;
+
+		try {
+
+			LinkedList<Jugador> jugadores = new LinkedList<Jugador>();
+			conn = DbConnector.getInstancia().getConn();
+			stmt = conn.prepareStatement("Select id_jugador, id_pais, usuario, nombre, apellido from partidas p "
+					+ "Inner join jugadores j on j.id = p.id_jugador "
+					+ "inner join juegos ju on ju.id = p.id_juego "
+					+ "where denominacion = ?");
+			stmt.setString(1, nombreJuego);
+			rs = stmt.executeQuery();
+			if (rs != null) {
+
+				while (rs.next()) {
+
+					Jugador jugador = new Jugador();
+					Pais pais = new Pais();
+					pais.setId(rs.getInt("id_pais"));
+					jugador.setId(rs.getInt("id"));
+					jugador.setUsuario(rs.getString("usuario"));
+					jugador.setNombre(rs.getString("nombre"));
+					jugador.setApellido(rs.getString("apellido"));
+					jugador.setPais(pais);
+					jugadores.add(jugador);	
 				}
 			}
-	
+			return jugadores;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (stmt!=null) stmt.close();
+				if (conn!=null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
-	
+
+
+
 }
--- Torneo/src/main/java/Datos/DataTipoTorneo.java
+++ Torneo/src/main/java/Datos/DataTipoTorneo.java
@@ -16,34 +16,34 @@ public class DataTipoTorneo {
 
 	// Listar 
 	public LinkedList<TipoTorneo> list(){
-		
+
 		LinkedList<TipoTorneo> tipos= new LinkedList<TipoTorneo>();
 		Statement stmt = null;
 		ResultSet rs = null;
 		Connection conn = null;
-		
+
 		try {
-			
+
 			conn = DbConnector.getInstancia().getConn();
-			
+
 			// Ejecutar querys
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery("SELECT * FROM tipo_torneo");
-			
+
 			while(rs.next()) /*Empieza apuntando en -1*/ {
-				
+
 				TipoTorneo tt = new TipoTorneo();
-				
+
 				tt.setId(rs.getInt("id"));
 				tt.setDenominacion(rs.getString("denominacion"));
 				tipos.add(tt);
 			}
-			
-		//cerrar conexion
-		if(rs!=null) {rs.close();}
-		if(stmt!=null) {stmt.close();}
-		conn.close();
-		
+
+			//cerrar conexion
+			if(rs!=null) {rs.close();}
+			if(stmt!=null) {stmt.close();}
+			conn.close();
+
 		}catch(SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
 		}finally {
@@ -55,44 +55,44 @@ public class DataTipoTorneo {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return tipos;
 	}
-	
+
 	//Búsqueda
 	public TipoTorneo search(String denominacion) {
-		
+
 		TipoTorneo tt = null;
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			// conexion
 			conn = DbConnector.getInstancia().getConn();
-			
+
 			stmt = conn.prepareStatement("SELECT * FROM tipo_torneo WHERE denominacion=?");
 			//setear parametros
 			stmt.setString(1, denominacion);
-			
+
 			tt = new TipoTorneo();
-			
+
 			//resultados
 			rs = stmt.executeQuery();
-			
+
 			//mapear
 			if(rs.next()) {
-				
+
 				tt.setId(rs.getInt("id"));
 				tt.setDenominacion(rs.getString("denominacion"));
 			}
-			
+
 			//cerrar conexion
 			if(rs!=null) {rs.close();}
 			if(stmt!=null) {stmt.close();}
-			
+
 			conn.close();
-			
+
 		}catch(SQLException ex){
 			System.out.println("SQLException: " + ex.getMessage());
 		}finally {
@@ -106,27 +106,27 @@ public class DataTipoTorneo {
 		}
 		return tt;
 	}
-	
+
 	public void create(TipoTorneo nuevoTipo) {
-		
+
 		Connection conn = null;
-		
+
 		ResultSet keyrs = null;
-		
+
 		PreparedStatement stmt = null;
-		
+
 		try {
 			conn = DbConnector.getInstancia().getConn();
 			stmt = conn.prepareStatement("insert into tipo_torneo (denominacion) values (?)", Statement.RETURN_GENERATED_KEYS);
 			stmt.setString(1, nuevoTipo.getDenominacion()); 
 			stmt.executeUpdate();
 			keyrs = stmt.getGeneratedKeys();
-			
+
 			if (keyrs != null && keyrs.next()) {
-				
+
 				nuevoTipo.setId(keyrs.getInt(1));
 			}	
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -134,106 +134,106 @@ public class DataTipoTorneo {
 				if (conn != null) conn.close();
 				if (stmt != null) stmt.close();
 				if (keyrs != null) keyrs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
+
 	//cargar
-//	public void create(String denominacion) {
-//		
-//		Connection conn = null;
-//		PreparedStatement pstmt = null;
-//		TipoTorneo ttNuevo = new TipoTorneo();
-//		ttNuevo.setDenominacion(denominacion);
-//		ResultSet keyrs = null;
-//		
-//		try {
-//			conn = DbConnector.getInstancia().getConn();
-//			pstmt = conn.prepareStatement("insert into tipo_torneo (denominacion) values (?)", Statement.RETURN_GENERATED_KEYS);
-//			pstmt.setString(1, ttNuevo.getDenominacion()); 
-//			pstmt.executeUpdate();
-//			keyrs = pstmt.getGeneratedKeys();
-//			
-//			if (keyrs != null && keyrs.next()) {
-//				
-//				ttNuevo.setId(keyrs.getInt(1));
-//			}	
-//			
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		} finally {
-//			try {	
-//				if (conn != null) conn.close();
-//				if (pstmt != null) pstmt.close();
-//				if (keyrs != null) keyrs.close();
-//				} catch (SQLException e) {
-//					e.printStackTrace();
-//				}
-//		}
-//	}
-	
+	//	public void create(String denominacion) {
+	//		
+	//		Connection conn = null;
+	//		PreparedStatement pstmt = null;
+	//		TipoTorneo ttNuevo = new TipoTorneo();
+	//		ttNuevo.setDenominacion(denominacion);
+	//		ResultSet keyrs = null;
+	//		
+	//		try {
+	//			conn = DbConnector.getInstancia().getConn();
+	//			pstmt = conn.prepareStatement("insert into tipo_torneo (denominacion) values (?)", Statement.RETURN_GENERATED_KEYS);
+	//			pstmt.setString(1, ttNuevo.getDenominacion()); 
+	//			pstmt.executeUpdate();
+	//			keyrs = pstmt.getGeneratedKeys();
+	//			
+	//			if (keyrs != null && keyrs.next()) {
+	//				
+	//				ttNuevo.setId(keyrs.getInt(1));
+	//			}	
+	//			
+	//		} catch (SQLException e) {
+	//			e.printStackTrace();
+	//		} finally {
+	//			try {	
+	//				if (conn != null) conn.close();
+	//				if (pstmt != null) pstmt.close();
+	//				if (keyrs != null) keyrs.close();
+	//				} catch (SQLException e) {
+	//					e.printStackTrace();
+	//				}
+	//		}
+	//	}
+
 	//borrar
 	public void delete(String denominacionEliminar) {
-		
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		
+
 		try {
 			// crear conexion
 			conn = DbConnector.getInstancia().getConn();
-			
+
 			//query
 			pstmt = conn.prepareStatement(
-			"DELETE FROM tipo_torneo WHERE denominacion = ?;" 
+					"DELETE FROM tipo_torneo WHERE denominacion = ?;" 
 					);
-			
+
 			pstmt.setString(1, denominacionEliminar);
 			pstmt.executeUpdate();
-			
+
 			if(pstmt!=null) {pstmt.close();}
 			conn.close();
-			
+
 		}catch(SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
 		}finally {
 			try {
-			if(pstmt!=null) {pstmt.close();}
-			conn.close();
+				if(pstmt!=null) {pstmt.close();}
+				conn.close();
 			}catch(SQLException e){
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	//actualizar
 	public void update(int id, String denominacion) {
-		
+
 		PreparedStatement pstmt = null;
 		Connection conn = null;
 		TipoTorneo ttNuevo = new TipoTorneo();
 		ttNuevo.setId(id);
 		ttNuevo.setDenominacion(denominacion);
-		
+
 		try {
 			// crear conexion
 			conn = DbConnector.getInstancia().getConn();
-			
+
 			//query
 			pstmt = conn.prepareStatement(
 					"Update tipo_torneo SET denominacion=? WHERE id=?;" 
 					);
-			
+
 			pstmt.setObject(1, ttNuevo.getDenominacion());
 			pstmt.setInt(2, ttNuevo.getId());
 			pstmt.executeUpdate();
-			
+
 			if(pstmt!=null) {pstmt.close();}
 			conn.close();
-			
-			
+
+
 		}catch(SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
 		}finally {
@@ -244,7 +244,7 @@ public class DataTipoTorneo {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 }
