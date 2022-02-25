7ace6ca760d2e0819be80fc851a744d516da7d82
juliiian99 <juliiianvilloria99@gmail.com>
2022-02-25 19:58:45 -0300

    .

--- Torneo/src/main/java/Servlets/EmailSendingServlet.java
+++ Torneo/src/main/java/Servlets/EmailSendingServlet.java
@@ -13,27 +13,11 @@ import Datos.EmailData;
  
 @WebServlet("/EmailSendingServlet")
 public class EmailSendingServlet extends HttpServlet {
-<<<<<<< HEAD
+
     private String host = "smtp.gmail.com";
     private String port = "587";
     private String user = "reportesappjava@gmail.com";
     private String pass = "Milanesas123";
-=======
-	private static final long serialVersionUID = 1L;
-	private String host;
-    private String port;
-    private String user;
-    private String pass;
- 
-    public void init() {
-        // reads SMTP server setting from web.xml file
-        ServletContext context = getServletContext();
-        host = context.getInitParameter("host");
-        port = context.getInitParameter("port");
-        user = context.getInitParameter("user");
-        pass = context.getInitParameter("pass");
-    }
->>>>>>> 844afa76c190d2a306f324457e43cd3d8b4dd4fe
     
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
